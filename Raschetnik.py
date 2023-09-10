5313920c09570e472457847e2ec3c0430b06300a
ulyavita <ulyana_vitman@mail.ru>
2023-09-10 18:56:46 +0700

    1.1

---
+++ Raschetnik.py
@@ -0,0 +1,17 @@
+import time
+a = list(input())
+b = int(input())
+
+start_t = time.time()
+for i in range(len(a) - 1, - 1, - 1):
+    a[i] = int(a[i]) * b
+for i in range(len(a) - 1, 0, - 1):
+    if a[i] > 9:
+        a[i - 1] = a[i - 1] + a[i] // 10
+        a[i] = a[i] % 10
+
+print(*a, sep="")
+print("Время", time.time() - start_t)
+
+
+\\for commit test
